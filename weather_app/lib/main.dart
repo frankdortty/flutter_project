e3cf7c68714bd3bc528148f2c252da06a2c81270
frankdortty <okoriefranklyn16@gmail.com>
2024-12-16 13:11:43 +0100

    first commit

---
+++ weather_app/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      getPages: [
+        GetPage(
+          name: '/',
+          page: () => HomeScreen(),
+          binding: HomeBinding(),
+        )
+      ],
+    );
+  }
+}
